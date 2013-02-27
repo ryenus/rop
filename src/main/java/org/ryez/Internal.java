160101c8728b212e15c0c894d9c199f949aca46d
ryenus <ryenus@gmail.com>
2013-02-28 02:01:32 +0800

    allow some non option fields if option field(s) exist

--- src/main/java/org/ryez/Internal.java
+++ src/main/java/org/ryez/Internal.java
@@ -26,25 +26,27 @@ class CommandInfo {
 		for (Field field : klass.getDeclaredFields()) {
 			if (!field.isSynthetic()) {
 				Option optAnno = field.getAnnotation(Option.class);
-				if (optAnno == null) {
-					throw new RuntimeException(String.format("Annotation @Option missing on field '%s' in class '%s'", field.getName(), klass.getName()));
-				}
-
-				String[] opts = optAnno.opt();
-				if (opts.length == 0) {
-					throw new RuntimeException(String.format("@Option.opt is empty on field '%s' in class '%s'", field.getName(), klass.getName()));
-				}
-
-				OptionInfo optionInfo = new OptionInfo(field, optAnno);
-				for (String opt : opts) {
-					String key = opt.replaceFirst("^(-)+", "");
-					if (map.containsKey(key)) {
-						throw new RuntimeException(String.format("Cannot use opt '%s' again on field '%s' in class '%s', already used on field '%s'",
-							opt, field.getName(), klass.getName(), map.get(key).field.getName()));
+				if (optAnno != null) {
+					String[] opts = optAnno.opt();
+					if (opts.length == 0) {
+						throw new RuntimeException(String.format("@Option.opt is empty on field '%s' in class '%s'", field.getName(), klass.getName()));
 					}
-					map.put(key, optionInfo);
+
+					OptionInfo optionInfo = new OptionInfo(field, optAnno);
+					for (String opt : opts) {
+						String key = opt.replaceFirst("^(-)+", "");
+						if (map.containsKey(key)) {
+							throw new RuntimeException(String.format("Cannot use opt '%s' again on field '%s' in class '%s', already used on field '%s'",
+								opt, field.getName(), klass.getName(), map.get(key).field.getName()));
+						}
+						map.put(key, optionInfo);
+					}
 				}
 			}
+		}
+
+		if (map.isEmpty()) {
+			throw new RuntimeException(String.format("Cannot find any field with annotation @Option in class '%s'", klass.getName()));
 		}
 	}
 

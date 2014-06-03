a93ec09eb817c393914bee8065315fe55a91f582
yann.diorcet <yann.diorcet@usharesoft.com>
2014-06-03 11:08:49 +0200

    Make CommandInfo & OptionInfo public

--- src/main/java/com/github/ryenus/rop/OptionInfo.java
+++ src/main/java/com/github/ryenus/rop/OptionInfo.java
@@ -4,18 +4,30 @@ import java.lang.reflect.Field;
 
 import com.github.ryenus.rop.OptionParser.Option;
 
-class OptionInfo {
+public class OptionInfo {
 	Field field;
 	Option anno;
 	boolean set;
 
-	OptionInfo(Field field, Option optAnno) {
+	public OptionInfo(Field field, Option optAnno) {
 		this.field = field;
 		this.anno = optAnno;
 		this.set = false;
 	}
 
-	String help() {
+	public Field getField() {
+		return field;
+	}
+
+	public Option getAnno() {
+		return anno;
+	}
+
+	public boolean isSet() {
+		return set;
+	}
+
+	public String help() {
 		String optsText = Utils.formatOpts(anno.opt());
 		String descText = Utils.format(anno.description(), true);
 		return String.format("%s  %s", optsText, descText);
